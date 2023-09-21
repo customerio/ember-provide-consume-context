c9b0a2403a3dd794b72dce264eac36dc78d0fca6
Kevin Kucharczyk <kevin@customer.io>
2023-09-21 16:17:07 +1000

    Fix utility type export

--- ember-context/src/index.ts
+++ ember-context/src/index.ts
@@ -1,2 +1,3 @@
 export { createContext } from './-private/create-context';
+export type { contextValueType } from './-private/create-context';
 export { inject } from './-private/inject';
--- test-app/app/components/test-component.ts
+++ test-app/app/components/test-component.ts
@@ -1,7 +1,7 @@
 import Component from '@glimmer/component';
 import { inject as context } from '@customerio/ember-context';
 import { NumberContext } from 'test-app/controllers/application';
-import { contextValueType } from '@customerio/ember-context/create-context-6359e4ca';
+import type { contextValueType } from '@customerio/ember-context';
 
 export default class TestComponent extends Component {
   @context(NumberContext) contextValue!: contextValueType<typeof NumberContext>;
