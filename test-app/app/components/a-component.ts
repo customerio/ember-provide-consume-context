0bf0e9fa787fef3274fb610f6e86f8ffb59b08f7
Kevin Kucharczyk <kevin@customer.io>
2023-09-21 15:53:57 +1000

    Fix lint errors

--- test-app/app/components/a-component.ts
+++ test-app/app/components/a-component.ts
@@ -1,3 +1,7 @@
 import Component from '@glimmer/component';
 
-export default class AComponent extends Component {}
+export default class AComponent extends Component {
+  get noop() {
+    return null;
+  }
+}
