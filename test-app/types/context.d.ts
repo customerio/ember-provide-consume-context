951f127414d91246e692f96955efa2f100bfcbea
Kevin Kucharczyk <kevin@customer.io>
2023-09-28 09:19:42 +1000

    More specific import

--- test-app/types/context.d.ts
+++ test-app/types/context.d.ts
@@ -1,5 +1,5 @@
 // Importing the module triggers TS declaration merging
-import '@customerio/ember-context';
+import '@customerio/ember-context/context-registry';
 
 // Define global context types here, or declare them in the modules where
 // they're provided.
