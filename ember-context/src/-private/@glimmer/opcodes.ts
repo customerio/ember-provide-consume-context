60ad5cdde34895e9eafecb869441d4c8de005d25
Kevin Kucharczyk <kevin@customer.io>
2023-09-28 15:27:31 +1000

    Better comment

--- ember-context/src/-private/@glimmer/opcodes.ts
+++ ember-context/src/-private/@glimmer/opcodes.ts
@@ -1,6 +1,10 @@
-// We can't install @glimmer/interfaces as a dependency, so we copy the opcodes
-// that are relevant to us.
+// We can't use enums from @glimmer/interfaces due to the issue below:
 // https://github.com/glimmerjs/glimmer-vm/issues/1294
+// We can use type imports, but an enum would need to be transformed into JS,
+// which fails.
+// We copy the opcodes from the source into here, and only the ones that are
+// relevant to us.
+// This is safe, because the opcodes should be stable.
 // https://github.com/glimmerjs/glimmer-vm/blob/68d371bdccb41bc239b8f70d832e956ce6c349d8/packages/%40glimmer/vm/lib/opcodes.ts#L196
 export const enum Op {
   GetComponentSelf = 90,
