19091b833ee10bc39f8af000ed6795caa257c1cf
Kevin Kucharczyk <kevin@customer.io>
2024-01-04 08:08:25 +1100

    Also import preview types

--- ember-provide-consume-context/unpublished-development-types/index.d.ts
+++ ember-provide-consume-context/unpublished-development-types/index.d.ts
@@ -2,6 +2,7 @@
 // These will *not* be published as part of your addon, so be careful that your published code does not rely on them!
 
 import 'ember-source/types';
+import 'ember-source/types/preview';
 import '@glint/environment-ember-loose';
 
 declare module '@glint/environment-ember-loose/registry' {
