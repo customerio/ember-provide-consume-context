2983d9212d113e618e494a428455d0a854fa5854
Kevin Kucharczyk <kevin@customer.io>
2024-01-04 10:18:37 +1100

    Add more tests

--- test-app/tests/integration/components/built-in-components-test.ts
+++ test-app/tests/integration/components/built-in-components-test.ts
@@ -6,6 +6,7 @@ import { tracked } from '@glimmer/tracking';
 
 class Context {
   @tracked count?: number;
+  @tracked hidden?: boolean;
 }
 
 interface ThisContext {
@@ -68,5 +69,130 @@ module('Integration | Built-in components', function (hooks) {
     context.count = 2;
     await settled();
     assert.dom('#content').hasText('2');
+  });
+
+  test("a consumer can't access a context it's not nested in", async function (assert) {
+    await render(hbs`
+      <ContextProvider @key="my-test-context-1" @value="1">
+        <ContextConsumer @key="my-test-context-1" as |count|>
+          <div id="content-1">{{count}}</div>
+        </ContextConsumer>
+      </ContextProvider>
+      <ContextProvider @key="my-test-context-2" @value="2">
+        <ContextConsumer @key="my-test-context-1" as |count|>
+          <div id="content-2">{{count}}</div>
+        </ContextConsumer>
+      </ContextProvider>
+    `);
+
+    assert.dom('#content-2').hasText('');
+  });
+
+  test('consuming context in conditional', async function (assert) {
+    context.count = 1;
+    context.hidden = false;
+
+    await render<ThisContext>(hbs`
+      <ContextProvider @key="my-test-context" @value={{this.context.count}}>
+        {{#unless this.context.hidden}}
+          <ContextConsumer @key="my-test-context" as |count|>
+            <div id="content">{{count}}</div>
+          </ContextConsumer>
+        {{/unless}}
+      </ContextProvider>
+    `);
+
+    assert.dom('#content').exists();
+    assert.dom('#content').hasText('1');
+
+    context.hidden = true;
+    await settled();
+    assert.dom('#content').doesNotExist();
+
+    context.hidden = false;
+    await settled();
+    assert.dom('#content').exists();
+    assert.dom('#content').hasText('1');
+
+    context.hidden = true;
+    await settled();
+    context.count = 2;
+    await settled();
+    context.hidden = false;
+    await settled();
+    assert.dom('#content').exists();
+    assert.dom('#content').hasText('2');
+  });
+
+  test('context provider in conditional', async function (assert) {
+    context.hidden = false;
+
+    await render<ThisContext>(hbs`
+      {{#unless this.context.hidden}}
+        <ContextProvider @key="my-test-context" @value="1">
+          <ContextConsumer @key="my-test-context" as |count|>
+            <div id="content">{{count}}</div>
+          </ContextConsumer>
+        </ContextProvider>
+      {{/unless}}
+    `);
+
+    assert.dom('#content').exists();
+    assert.dom('#content').hasText('1');
+
+    context.hidden = true;
+    await settled();
+    assert.dom('#content').doesNotExist();
+
+    context.hidden = false;
+    await settled();
+    assert.dom('#content').exists();
+    assert.dom('#content').hasText('1');
+  });
+
+  test('consuming context with another provider in conditional sibling', async function (assert) {
+    context.hidden = true;
+
+    await render<ThisContext>(hbs`
+      <ContextProvider @key="my-test-context" @value="1">
+        {{#unless this.context.hidden}}
+          <ContextProvider @key="my-test-context" @value="2">
+          </ContextProvider>
+        {{/unless}}
+
+        <ContextConsumer @key="my-test-context" as |count|>
+          <div id="content">{{count}}</div>
+        </ContextConsumer>
+      </ContextProvider>
+    `);
+
+    assert.dom('#content').hasText('1');
+
+    context.hidden = false;
+    await settled();
+    assert.dom('#content').hasText('1');
+
+    context.hidden = true;
+    await settled();
+    assert.dom('#content').hasText('1');
+  });
+
+  test('nesting different contexts', async function (assert) {
+    await render(hbs`
+      <ContextProvider @key="my-test-context" @value="1">
+        <ContextProvider @key="my-test-context-2" @value="2">
+          <ContextConsumer @key="my-test-context" as |contextOne|>
+            <div id="content-1">{{contextOne}}</div>
+          </ContextConsumer>
+
+          <ContextConsumer @key="my-test-context-2" as |contextTwo|>
+            <div id="content-2">{{contextTwo}}</div>
+          </ContextConsumer>
+        </ContextProvider>
+      </ContextProvider>
+    `);
+
+    assert.dom('#content-1').hasText('1');
+    assert.dom('#content-2').hasText('2');
   });
 });
