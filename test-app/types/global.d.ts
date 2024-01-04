39c5c91b2e1c7516802d6dd8d46ad84800b3691e
Kevin Kucharczyk <kevin@customer.io>
2024-01-04 14:17:19 +1100

    Remove unused lines

--- test-app/types/global.d.ts
+++ test-app/types/global.d.ts
@@ -1,5 +1,3 @@
-// import 'ember-source/types';
-// import 'ember-source/types/preview';
 import '@glint/environment-ember-loose';
 
 import type EmberContextTemplateRegistry from 'ember-provide-consume-context/template-registry';
