24ade376999739a0791c6f324979ad4125af7db4
Kevin Kucharczyk <kevin@customer.io>
2026-06-03 09:16:28 +1000

    Run live Ember try scenarios in Ember 7 app

---
+++ test-app-ember-7/config/ember-try.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const getChannelURL = require('ember-source-channel-url');
+
+module.exports = async function () {
+  return {
+    command: 'pnpm test:ember',
+    packageManager: 'pnpm',
+    scenarios: [
+      {
+        name: 'ember-release',
+        npm: {
+          devDependencies: {
+            'ember-source': await getChannelURL('release'),
+          },
+        },
+      },
+      {
+        name: 'ember-beta',
+        npm: {
+          devDependencies: {
+            'ember-source': await getChannelURL('beta'),
+          },
+        },
+      },
+      {
+        name: 'ember-canary',
+        npm: {
+          devDependencies: {
+            'ember-source': await getChannelURL('canary'),
+          },
+        },
+      },
+    ],
+  };
+};
